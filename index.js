20949753fb133c1de3f692b8f6d2ea279f1b5d48
rocio0819 <111366772+rocio0819@users.noreply.github.com>
2022-11-24 08:21:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const router = require("./routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/css', express.static(`${__dirname}/static/css`));
+app.use('/js', express.static(`${__dirname}/static/js`));
+app.use('/img', express.static(`${__dirname}/static/img`));
+
+app.use("/", router)
+
+app.listen(port, () => {
+    console.log(`Frontend ${port}`);
+})
